9b83a553c52a0220bf582a93887bc50e19d1afe9
AlanGarcia <garciaalan0421@gmail.com>
2026-01-07 14:28:38 -0600

    Add user model with schema for MongoDB

---
+++ backend/models/user.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true    // Username must be unique
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true    // Email must be unique
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    followers: [    // List of user IDs who follow this user of type ObjectId referencing User model
+        { 
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            default: [] // Default to empty array
+        }
+    ],
+    following: [
+        { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User',
+            default: [] // Default to empty array
+        }
+    ], 
+    profileImg: {
+        type: String,
+        default: "" // Default to empty string if no profile image is provided
+    },
+    coverImg: {
+        type: String,
+        default: "" // Default to empty string if no cover image is provided
+    },
+    bio: {
+        type: String,
+        default: "" // Default to empty string if no bio is provided
+    },
+    link: {
+        type: String,
+        default: "" // Default to empty string if no link is provided
+    }
+
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
