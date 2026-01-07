41634e8ab6d7b27d7edc5a4af7d34a31a8e6665c
AlanGarcia <garciaalan0421@gmail.com>
2026-01-07 13:00:24 -0600

    Tested app port listening and configuration

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("Server is running on port 5000");
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+app.listen(PORT || 5000, () => {
+  console.log(`Server is running on http://localhost:${PORT || 5000}`);
+});
