54d482eda9c8bff78c16fdc6c157fa56cb1f5c26
AlanGarcia <garciaalan0421@gmail.com>
2026-01-07 13:45:22 -0600

    Included MongoDB connection function to app.listen

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js'; // Using .js extension because of ES module
+import connectMongoDB from './db/connectMongoDB.js';
 
 const app = express();
 
@@ -11,5 +12,6 @@ const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT || 5000, () => {
-  console.log(`Server is running on http://localhost:${PORT || 5000}`);
+    connectMongoDB();
+    console.log(`Server is running on http://localhost:${PORT || 5000}`);
 });
