433426a8a8eab0d688843900387a89c8ad77e7b9
Alan Garcia <garciaalan0421@gmail.com>
2026-01-12 15:28:39 -0600

    Initial commit of post controllers

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,6 @@
+import Post from '../models/post.model.js'
+
+export const createPost = async (req, res) => {};
+export const likeUnlikePostPost = async (req, res) => {};
+export const commentOnPostPost = async (req, res) => {};
+export const deletePostPost = async (req, res) => {};
