782754dcd273c74fe2c5f6be5ce06c30b437b821
Alan Garcia <garciaalan0421@gmail.com>
2026-01-12 17:24:27 -0600

    Finished implementing createPost controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,40 @@
+import { json } from 'express';
+import {v2 as cloudinary} from 'cloudinary';
 import Post from '../models/post.model.js'
+import User from '../models/user.model.js';
 
-export const createPost = async (req, res) => {};
-export const likeUnlikePostPost = async (req, res) => {};
-export const commentOnPostPost = async (req, res) => {};
-export const deletePostPost = async (req, res) => {};
+export const createPost = async (req, res) => {
+    try {
+        const { text } = req.body
+        let { img } = req.body
+        
+        const user = await User.findById(req.user._id)
+
+        if(!user) return res.status(404).json({ error: "User not found" });
+
+        if(!text && !img) return res.status(400).json({ error: "Post must have text or image" });
+
+        if(img){
+            const uploadedResponse = await cloudinary.uploader.upload(img);
+            img = uploadedResponse.secure_url;
+        }
+
+        const newPost = new Post({
+            user: req.user._id,
+            text,
+            img
+        })
+
+        await newPost.save();
+        res.status(201).json({ message: "Post uploaded successfully", data: newPost });
+
+
+
+    } catch (error) {
+        console.log("Error in createPost controller: ", error);
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+};
+export const likeUnlikePost = async (req, res) => {};
+export const commentOnPost = async (req, res) => {};
+export const deletePost = async (req, res) => {};
