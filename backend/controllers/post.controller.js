9973ee0c13e43551476b2fd22f7bfadcab77e9b2
AlanGarcia <garciaalan0421@gmail.com>
2026-01-12 23:15:49 -0600

    Finished implementing likeUnlikePost

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,7 +1,7 @@
-import { json } from 'express';
 import {v2 as cloudinary} from 'cloudinary';
-import Post from '../models/post.model.js'
+import Post from '../models/post.model.js';
 import User from '../models/user.model.js';
+import Notification from '../models/notification.model.js';
 
 export const createPost = async (req, res) => {
     try {
@@ -35,6 +35,44 @@ export const createPost = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" })
     }
 };
-export const likeUnlikePost = async (req, res) => {};
+export const likeUnlikePost = async (req, res) => {
+    /*
+        The id param will be the Post Object ID
+        Will check if the user has already liked said post (if not, unlike and vice versa)
+        Update Post.likes array by appending user's ID that liked the post
+    */
+    const { id } = req.params
+    const userId = req.user._id
+
+    try {
+        const post = await Post.findById(id)
+
+        if(!post) return res.status(404).json({ error: "Post not found" });
+        if(post.user.equals(userId)) return res.status(404).json({ error: "Cannot like your own post" });
+
+        const hasLiked = post.likes.includes(userId.toString())
+
+        if(!hasLiked){
+            await Post.findByIdAndUpdate(id, { $push: { likes: userId} })
+
+            const newNotification = new Notification({
+                from: userId,
+                to: post.user,
+                type: 'like'
+            })
+
+            await newNotification.save();
+
+            res.status(200).json({ message: "Liked post successfuly" })
+        }
+        else{
+            await Post.findByIdAndUpdate(id, { $pull: { likes: userId} })
+            res.status(200).json({ message: "Unliked post successfuly" })
+        }
+    } catch (error) {
+        console.log("Error in likeUnlikePost controller: ", error.message);
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+};
 export const commentOnPost = async (req, res) => {};
 export const deletePost = async (req, res) => {};
