75f4890659cf9e8221d0f6457f26bb7d98b19228
AlanGarcia <garciaalan0421@gmail.com>
2026-01-14 00:13:19 -0600

    Added three get requests to post route

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost, likeUnlikePost, commentOnPost, deletePost} from "../controllers/post.controller.js"
+import { createPost, likeUnlikePost, commentOnPost, deletePost, getAllPosts, getLikedPosts, getFollowingPosts } from "../controllers/post.controller.js"
 
 const router = express.Router()
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/following", protectRoute, getFollowingPosts)
 
 router.post('/create', protectRoute, createPost);
 router.post('/like/:id', protectRoute, likeUnlikePost);
