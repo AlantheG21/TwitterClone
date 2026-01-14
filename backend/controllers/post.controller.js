f6cecf6b5774ccda136e090524e0313f39d7f4f2
AlanGarcia <garciaalan0421@gmail.com>
2026-01-13 23:05:46 -0600

    Finished implementing deletePost and commentOnPost controllers

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -74,5 +74,46 @@ export const likeUnlikePost = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" })
     }
 };
-export const commentOnPost = async (req, res) => {};
-export const deletePost = async (req, res) => {};
+export const commentOnPost = async (req, res) => {
+    try {
+        const { text } = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id
+
+        if(!text) return res.status(404).json({ error: "Text field is required" });
+
+        const post = await Post.findById(postId);
+
+        if(!post) return res.status(404).json({ error: "Post not found" });
+
+        const comment = {user: userId, text};
+
+        post.comments.push(comment)
+
+        await post.save();
+        res.status(200).json({ message: "Commented successfully", comment });
+    } catch (error) {
+        console.log("Error in commentOnPost controller: ", error.message);
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+};
+export const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if(!post) return res.status(404).json({ error: "Post not found" });
+
+        if(!post.user.equals(req.user._id)) return res.status(401).json({ error: "You are not authorized to delete this post" });
+
+        if(post.img){
+            const img = post.img.split("/").pop().split(".")[0]
+            await cloudinary.uploader.destroy(img);
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Post deleted successfully" })
+    } catch (error) {
+        console.log("Error in deletePost controller: ", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
