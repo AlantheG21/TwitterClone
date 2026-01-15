ab12aea82c78d9502b4d03d8bbbcfa9b98bb5daa
AlanGarcia <garciaalan0421@gmail.com>
2026-01-14 23:39:33 -0600

    Finished implementing deleteNotification controller

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -31,3 +31,22 @@ export const deleteNotifications = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+export const deleteNotification = async (req, res) => {
+    const notificationId = req.params.id;
+    const userId = req.user._id;
+    
+    try {
+        const notification = await Notification.findOne({ _id: notificationId, to: userId });
+        if (!notification) {
+            return res.status(404).json({ error: "Notification not found" });
+        }
+
+        await Notification.deleteOne({ _id: notificationId });
+
+        res.status(200).json({ message: "Notification deleted successfully" });
+    } catch (error) {
+        console.log("Error in deleteNotification controller: ", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
