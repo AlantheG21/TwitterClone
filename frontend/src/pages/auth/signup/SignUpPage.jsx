22ec22537ff75b58fa27d860e18f1a2f661e61eb
Alan Garcia <garciaalan0421@gmail.com>
2026-01-17 12:49:00 -0600

    Started on sign up page layout

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -1,6 +1,22 @@
+import XSvg from "../../../components/svgs/X"
+
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <div className="max-w-screen-xl mx-auto flex h-screen px-10">
+      <div className="flex-1 hidden lg:flex justify-center items-center">
+        <XSvg className="lg:w-2/3 fill-white" />
+      </div>
+      <div className="flex-1 flex flex-col justify-center">
+        <form className="lg:w-2/3 mx-auto md:mx--20 flex gap-4 flex-col">
+          <XSvg className="w-24 lg:hidden fill-white" />
+          <h1 className="text-4xl font-extrabold text-white">Join today</h1>
+          <label className="input input-bordered rounded flex items-center gap-2">
+            
+          </label>
+        </form>
+      </div>
+
+    </div>
   )
 }
 
